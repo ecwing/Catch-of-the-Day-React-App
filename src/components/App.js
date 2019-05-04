c51606d825644a81cb4c53dfb37d1310604d0634
ecwing <ericcwing@gmail.com>
2019-05-04 19:15:17 -0400

    Updating the order state

--- src/components/App.js
+++ src/components/App.js
@@ -29,12 +29,23 @@ addFish = (fish) => {
         fishes: fishes
     });
 };
+
 loadSampleFishes = () => {
     this.setState({ 
         fishes: sampleFishes 
     });
 };
 
+addToOrder = key => {
+    //1. take a copy of state
+    const order = {...this.state.order};
+    //2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setState to update our state object
+    this.setState({ 
+        order 
+    });
+}
 
 
     render() {
@@ -43,7 +54,11 @@ loadSampleFishes = () => {
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                        {Object.keys(this.state.fishes).map(key => <Fish 
+                            key={key} 
+                            index={key}
+                            details={this.state.fishes[key]}
+                            addToOrder={this.addToOrder}/>)}
                     </ul>
                     </div>
                     <Order />
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
+    //can do it inline if you only have to do it once above ^^
+    
     render() {
         const { image, name, price, desc, status } = this.props.details;
-
+        const isAvailable = status === 'available';
         return (
             <li className="menu-fish">
                 <img src={image} alt={name}/>
@@ -13,7 +18,9 @@ class Fish extends Component {
                 <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button>Add To Cart</button>
+                <button disabled={!isAvailable} onClick={this.handleClick}>
+                {isAvailable ? "Add To Order" : "Sold Out"}
+                </button>
             </li>
         )
     }
