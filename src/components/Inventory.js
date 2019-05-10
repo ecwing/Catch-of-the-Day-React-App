00d787d1b4f4efe8be12ffd7245b140879c296e8
ecwing <ericcwing@gmail.com>
2019-05-09 23:10:25 -0400

    inventory change

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 import firebase from "firebase";
-import AddFishForm from './AddFishForm';
+import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
-import Login from './Login';
+import Login from "./Login";
 import base, { firebaseApp } from "../base";
 
-class Inventory extends Component {
+class Inventory extends React.Component {
     static propTypes = {
-        fishes:  PropTypes.object,
+        fishes: PropTypes.object,
         updateFish: PropTypes.func,
         deleteFish: PropTypes.func,
-        loadSampleFishes: PropTypes.func
+        loadSampleFishes: PropTypes.func,
+        addFish: PropTypes.func
     };
 
     state = {
@@ -21,25 +22,25 @@ class Inventory extends Component {
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
-            if(user) {
+            if (user) {
                 this.authHandler({ user });
             }
         });
     }
 
     authHandler = async authData => {
-        // 1. look up the current store in the firebase database
+        // 1 .Look up the current store in the firebase database
         const store = await base.fetch(this.props.storeId, { context: this });
         console.log(store);
-        // 2. claim it if there is no owner
-        if(!store.owner) {
+        // 2. Claim it if there is no owner
+        if (!store.owner) {
             // save it as our own
             await base.post(`${this.props.storeId}/owner`, {
                 data: authData.user.uid
             });
         }
-        // 3. set the state of the inventory component to reflect the current user
-        this.setState = ({
+        // 3. Set the state of the inventory component to reflect the current user
+        this.setState({
             uid: authData.user.uid,
             owner: store.owner || authData.user.uid
         });
@@ -54,17 +55,19 @@ class Inventory extends Component {
     };
 
     logout = async () => {
-        console.log('Logging Out!');
+        console.log("Logging out!");
         await firebase.auth().signOut();
-        this.setState({ uid: null});
+        this.setState({ uid: null });
     };
 
     render() {
-        const logout = <button onClick={this.logout}>Log Out!</button>
-        //  1. check if they are logged in
+        const logout = <button onClick={this.logout}>Log Out!</button>;
+
+        // 1. Check if they are logged in
         if (!this.state.uid) {
-        return <Login authenticate={this.authenticate} />;
+            return <Login authenticate={this.authenticate} />;
         }
+
         // 2. check if they are not the owner of the store
         if (this.state.uid !== this.state.owner) {
             return (
@@ -74,24 +77,25 @@ class Inventory extends Component {
                 </div>
             );
         }
+
         // 3. They must be the owner, just render the inventory
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
                 {logout}
                 {Object.keys(this.props.fishes).map(key => (
-                    <EditFishForm 
-                    key={key}
-                    index={key} 
-                    fish={this.props.fishes[key]} 
-                    updateFish={this.props.updateFish}
-                    deleteFish={this.props.deleteFish} 
+                    <EditFishForm
+                        key={key}
+                        index={key}
+                        fish={this.props.fishes[key]}
+                        updateFish={this.props.updateFish}
+                        deleteFish={this.props.deleteFish}
                     />
                 ))}
                 <AddFishForm addFish={this.props.addFish} />
                 <button onClick={this.props.loadSampleFishes}>
-                Load Sample Fishes
-                </button>
+                    Load Sample Fishes
+        </button>
             </div>
         );
     }
